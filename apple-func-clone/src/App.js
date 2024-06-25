8df518c64eef210c9e5f380aae1049c4de99a458
LamesginewAndargie <lame2002@gmail.com>
2024-06-25 11:30:14 +0300

    final

---
+++ apple-func-clone/src/App.js
@@ -0,0 +1,26 @@
+import Header from "./Components/Header/Header";
+import Alert from "./Components/AlertSection/Alert";
+import FirstSection from "./Components/FirstSection/FirstSection";
+import SecondSection from "./Components/SecondSection/SecondSection";
+import ThirdSection from "./Components/ThirdSection/ThirdSection";
+import FourthSection from "./Components/FourthSection/FourthSection";
+import FivthSection from "./Components/FivthSection/FivthSection";
+import SixthSection from "./Components/SixthSection/SixthSection";
+import Footer from "./Components/Footer/Footer";
+function App() {
+	return (
+		<>
+			<Header />
+			<Alert />
+			<FirstSection />
+			<SecondSection />
+			<ThirdSection />
+			<FourthSection />
+			<FivthSection />
+			<SixthSection />
+			<Footer />
+		</>
+	);
+}
+
+export default App;
